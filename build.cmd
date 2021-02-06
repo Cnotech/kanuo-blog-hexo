41477fa9a30e4a64eb371d4c153e668f5b87f1f2
Cno <dsyourshy@qq.com>
2021-02-06 15:44:34 +0800

    20210206_15443379

--- build.cmd
+++ build.cmd
@@ -6,25 +6,29 @@ git add *.cmd
 git add *.js
 git add *.jpg
 git add *.png
+git add *.txt
+git add *.yml
+git add *.php
+git add *.html
 git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%"
 git push
 
 title Building(2/5)
 del /f /s /q public
 rd /s /q public
-xcopy /s stuff public
+xcopy /s stuff public\
 
 cmd /c "hexo g"
 
 title Compressing(3/5)
 cd public
 "C:\Program Files\7-Zip\7z.exe" a -tzip dist.zip *
-pause
 
-ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;rm -rf *"
+ssh root@148.70.88.72 "cd /www/admin/www.edgeless.top_80/wwwroot;rm -rf *"
 title Uploading(4/5)
-scp "D:\Desktop\edgeless-wiki-vuepress\docs\.vuepress\dist\dist.zip" root@148.70.88.72:/www/admin/wiki.edgeless.top_80/wwwroot/v2
+scp "dist.zip" root@148.70.88.72:/www/admin/wiki.edgeless.top_80/wwwroot/v2
 title Unziping(5/5)
-ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;unzip ./dist.zip"
+ssh root@148.70.88.72 "cd /www/admin/www.edgeless.top_80/wwwroot;unzip ./dist.zip"
 title Finish
 timeout 5
+pause
