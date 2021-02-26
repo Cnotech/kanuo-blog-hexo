dc206c9fede30be3a21b926d2dacee206265fd7f
Cno <dsyourshy@qq.com>
2021-02-27 00:39:32 +0800

    20210227_ 0393269

--- build.cmd
+++ build.cmd
@@ -27,10 +27,10 @@ title Compressing(3/5)
 cd public
 "C:\Program Files\7-Zip\7z.exe" a -tzip dist.zip *
 
-ssh root@148.70.88.72 "cd /www/admin/www.edgeless.top_80/wwwroot;rm -rf *"
+ssh root@192.144.230.47 "cd /www/wwwroot/www.edgeless.top;rm -rf *"
 title Uploading(4/5)
-scp "dist.zip" root@148.70.88.72:/www/admin/www.edgeless.top_80/wwwroot
+scp "dist.zip" root@192.144.230.47:/www/wwwroot/www.edgeless.top
 title Unziping(5/5)
-ssh root@148.70.88.72 "cd /www/admin/www.edgeless.top_80/wwwroot;unzip -O CP936 ./dist.zip"
+ssh root@192.144.230.47 "cd /www/wwwroot/www.edgeless.top;unzip -O CP936 ./dist.zip"
 title Finish
 timeout 5
